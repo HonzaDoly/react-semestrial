f5a90846fbd9225c2f9eaf9e9b9c38ecd3dd87b3
Jan Doleček <dolj14@vse.cz>
2024-05-29 13:35:56 +0200

    prisma types attached to carList and CarItem

--- src/components/CarItem.tsx
+++ src/components/CarItem.tsx
@@ -1,6 +1,7 @@
+import { CarWithDeps } from '@/types/prismaTypes'
 import { Car } from '@prisma/client'
 
-const CarItem = ({ car }: { car: Car }) => {
+const CarItem = ({ car }: { car: CarWithDeps }) => {
   return <div>{car.id}</div>
 }
 
