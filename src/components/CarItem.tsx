f7fe2fe89cabfece9e1ed6b226870f4e2bc9d0a8
Jan Doleček <dolj14@vse.cz>
2024-05-29 13:21:00 +0200

    CarList and CarItem setup

---
+++ src/components/CarItem.tsx
@@ -0,0 +1,9 @@
+import { Car } from "@prisma/client"
+
+const CarItem = ({car}: {car:Car}) => {
+    return(
+        <div>{car.id}</div>
+    )
+}
+
+export default CarItem
---
+++ src/components/CarList.tsx
@@ -0,0 +1,17 @@
+import {Car} from '@prisma/client'
+import CarItem from './CarItem'
+
+type Props = {
+    cars: Car[]
+}
+
+const CarList = ({cars}: Props) => {
+    return(
+        <div>
+            {cars.map(car => (
+            <CarItem key={car.id} car = {car}/>))}
+        </div>
+    )
+}
+
+export default CarList
