d54d420a5e3ceaf6defdd5dff2e67487e0db7ca8
Jan Doleček <dolj14@vse.cz>
2024-06-03 22:27:45 +0200

    fix redirect

--- src/utils/actions.ts
+++ src/utils/actions.ts
@@ -1,33 +1,33 @@
 "use server"
 
-import { redirect } from "next/navigation"
-import prisma from "./prisma"
+import { redirect } from "next/navigation";
+import prisma from "./prisma";
 
 export const createCar = async (formData: FormData) => {
-    const modelId = formData.get("modelId")?.toString()
-    const brandId = formData.get("brandId")?.toString()
-    const location = formData.get("location")?.toString()
-    const price = formData.get("price")?.toString()
-    const priceInt = price !== undefined ? parseInt(price, 10) : undefined
-    const year = formData.get("year")?.toString()
-    const yearInt = year !== undefined ? parseInt(year, 10) : undefined
-    const color = formData.get("color")?.toString()
-    const description = formData.get("description")?.toString()
+  const modelId = formData.get("modelId")?.toString();
+  const brandId = formData.get("brandId")?.toString();
+  const location = formData.get("location")?.toString();
+  const price = formData.get("price")?.toString();
+  const priceInt = price !== undefined ? parseInt(price, 10) : undefined;
+  const year = formData.get("year")?.toString();
+  const yearInt = year !== undefined ? parseInt(year, 10) : undefined;
+  const color = formData.get("color")?.toString();
+  const description = formData.get("description")?.toString();
 
-    if(!modelId || !brandId || !description){
-        return
-    }
-    
-    await prisma.car.create({
-        data:{
-            modelId: modelId,
-            brandId: brandId,
-            description: description,
-            year: yearInt,
-            color: color,
-            price: priceInt,
-            location: location,
-        }
-    })
-    redirect('/')
-}
+  if (!modelId || !brandId || !description) {
+    return;
+  }
+
+  await prisma.car.create({
+    data: {
+      modelId: modelId,
+      brandId: brandId,
+      description: description,
+      year: yearInt,
+      color: color,
+      price: priceInt,
+      location: location,
+    },
+  });
+  redirect("/");
+};
